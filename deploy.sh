5e488af23a6d86131fe95a8aef5fe2836249011b
Jan van Hellemond <jan@frontlab.nl>
2018-05-14 07:48:21 +0200

    Removed CSV meta tags.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 # Home
 aws s3 cp index.html s3://eventstack.nl/index.html --content-type "text/html; charset=utf-8" --cache-control "public, must-revalidate"
 
-# FronteersConf
+# Speaker dahboard and guest RSVP
 for f in fronteersconf/*.html fronteersconf/rsvp/*.html
 do
 	aws s3 cp "$f" "s3://eventstack.nl/${f%.*}" --content-type "text/html; charset=utf-8" --cache-control "private, must-revalidate"
 done
 
 # Static
-for f in static/* static/bitmaps/* static/vectors/*
+for f in fronteersconf/vcard/* static/* static/bitmaps/* static/vectors/*
 do
 	if [ ! -d "$f" ]
 	then
