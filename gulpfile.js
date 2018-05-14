3766c0b7c7cee6f474b039d9ef9bdc37ef2c48ff
Jan van Hellemond <jan@frontlab.nl>
2018-05-14 14:21:38 +0200

    Gulped.

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+var clean = require("gulp-clean");
+var htmlmin = require("gulp-htmlmin");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+
+
+gulp.task("clean", function() {
+	return gulp.src("./target", {read: false})
+	.pipe(clean());
+});
+
+gulp.task("html", function() {
+	return gulp.src("./source/**/*.html")
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest("./target/"));
+});
+
+gulp.task("css", function() {
+	return gulp.src("./source/static/*.scss")
+		.pipe(sass({outputStyle: "compressed"}))
+		.pipe(autoprefixer({browsers: ["last 2 versions"]}))
+		.pipe(gulp.dest("./target/static/"));
+});
+
+gulp.task("js", function() {
+	return pump([
+		gulp.src("./source/static/*.js"),
+		uglify(),
+		gulp.dest("./target/static/")
+	]);
+});
+
+gulp.task("assets", function() {
+	return gulp.src("./source/**/*.(ico|jpeg|png|svg|vcf)")
+	.pipe(gulp.dest("./target/"));
+});
+
+gulp.task(
+	"default",
+	gulp.series(
+		"clean",
+		gulp.parallel("html", "css", "js", "assets")
+	)
+);
