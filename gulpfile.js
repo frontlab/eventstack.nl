d805f30c0b9ad56f5a3c0d5c58f0c08120897a09
Jan van Hellemond <jan@frontlab.nl>
2019-03-14 21:55:03 +0100

    Unspringified.

--- gulpfile.js
+++ gulpfile.js
@@ -8,41 +8,38 @@ var sass = require("gulp-sass");
 var uglify = require("gulp-uglify");
 
 gulp.task("clean", function() {
-	return gulp.src("./target", {allowEmpty: true, read: false})
+	return gulp.src("target", {allowEmpty: true, read: false})
 	.pipe(clean());
 });
 
 gulp.task("html", function() {
-	return gulp.src("./source/**/*.html")
+	return gulp.src("source/**/*.html")
 	.pipe(include())
 	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest("./target/"));
+	.pipe(gulp.dest("target"));
 });
 
-gulp.task("css", function() {
-	return gulp.src("./source/static/**/*.scss")
+gulp.task("script", function() {
+	return gulp.src("source/static/**/*.js")
+	.pipe(uglify())
+	.pipe(rename({suffix: ".min"}))
+	.pipe(gulp.dest("target/static"));
+});
+
+gulp.task("styles", function() {
+	return gulp.src("source/static/**/*.scss")
 	.pipe(sass({outputStyle: "compressed"}))
 	.pipe(autoprefixer({browsers: ["last 2 versions"]}))
 	.pipe(rename({suffix: ".min"}))
-	.pipe(gulp.dest("./target/static/"));
-});
-
-gulp.task("js", function() {
-	return gulp.src("./source/static/**/*.js")
-	.pipe(uglify())
-	.pipe(rename({suffix: ".min"}))
-	.pipe(gulp.dest("./target/static/"));
+	.pipe(gulp.dest("target/static"));
 });
 
 gulp.task("assets", function() {
-	return gulp.src("./source/**/*.{ico,jpeg,png,svg,vcf}")
-	.pipe(gulp.dest("./target/"));
+	return gulp.src("source/**/*.{ico,jpeg,png,svg,vcf}")
+	.pipe(gulp.dest("target"));
 });
 
-gulp.task(
-	"default",
-	gulp.series(
-		"clean",
-		gulp.parallel("html", "css", "js", "assets")
-	)
-);
+gulp.task("default", gulp.series(
+	"clean",
+	gulp.parallel("html", "script", "styles", "assets")
+));
