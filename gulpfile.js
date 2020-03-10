d960833d666aae8d58e9356fcdaf4e64ca8a83f7
Jan van Hellemond <jan@frontlab.nl>
2020-03-10 13:10:02 +0100

    Redid.

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,69 @@
-var gulp = require("gulp");
-var autoprefixer = require("gulp-autoprefixer");
-var clean = require("gulp-clean");
-var htmlmin = require("gulp-htmlmin");
-var include = require("gulp-include");
-var rename = require("gulp-rename");
-var sass = require("gulp-sass");
-var uglify = require("gulp-uglify");
+const fs = require("fs");
+const gulp = require("gulp");
+const rimraf = require("rimraf");
 
-gulp.task("clean", function() {
-	return gulp.src("target", {allowEmpty: true, read: false})
-	.pipe(clean());
+// Gulp plugins:
+const htmlmin = require("gulp-htmlmin");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+
+// Settings for Gulp plugins:
+const settings = {
+	htmlmin: {
+		collapseWhitespace: true,
+		customAttrCollapse: /data-.*/,
+		processScripts: ["text/html"]
+	},
+	postcss: [require("autoprefixer")],
+	sass: {outputStyle: "compressed"}
+};
+
+// Empty the build folder (by deleting and then recreating it):
+gulp.task("clean", function(callback) {
+	rimraf("build", function() {
+		fs.mkdir("build", {recursive: true}, callback);
+	});
+});
+
+// Copy files from `assets/_root`:
+gulp.task("root", function() {
+	return gulp.src("assets/_root/**/*")
+	.pipe(gulp.dest("build"));
 });
 
 gulp.task("html", function() {
-	return gulp.src("source/**/*.html")
-	.pipe(include())
-	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest("target"));
+	return gulp.src([
+		"templates/**/*.html",
+		"!templates/**/_*/**/*",
+		"!templates/**/_*"
+	])
+	.pipe(htmlmin(settings.htmlmin))
+	.pipe(gulp.dest("build"));
 });
 
 gulp.task("script", function() {
-	return gulp.src("source/static/**/*.js")
+	return gulp.src("assets/*.js")
 	.pipe(uglify())
 	.pipe(rename({suffix: ".min"}))
-	.pipe(gulp.dest("target/static"));
+	.pipe(gulp.dest("build/assets"))
 });
 
 gulp.task("styles", function() {
-	return gulp.src("source/static/**/*.scss")
-	.pipe(sass({outputStyle: "compressed"}))
-	.pipe(autoprefixer({browsers: ["last 2 versions"]}))
+	return gulp.src("assets/*.scss")
+	.pipe(sass(settings.sass))
+	.pipe(postcss(settings.postcss))
 	.pipe(rename({suffix: ".min"}))
-	.pipe(gulp.dest("target/static"));
+	.pipe(gulp.dest("build/assets"));
 });
 
-gulp.task("assets", function() {
-	return gulp.src("source/**/*.{ico,jpeg,png,svg,vcf}")
-	.pipe(gulp.dest("target"));
+gulp.task("images", function() {
+	return gulp.src("assets/images/**/*")
+	.pipe(gulp.dest("build/assets/images"));
 });
 
 gulp.task("default", gulp.series(
 	"clean",
-	gulp.parallel("html", "script", "styles", "assets")
+	gulp.parallel("root", "html", "script", "styles", "images")
 ));
